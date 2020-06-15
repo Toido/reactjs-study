9df83a78cacd60c2e02dbc5a867ba597b0f849bf
darylcaguia <caguiadaryl@gmail.com>
2020-06-15 18:14:12 +0800

    Added CSS modules in App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import React, { useState } from 'react'; for Hook
 import React, { Component } from 'react';
-import './App.css';
+import classes from './myApp.module.css';
 
 import Person from './Person/Person';
 //import styled from 'styled-components';
@@ -103,6 +103,8 @@ class App extends Component {
     //========================================
 
     let displayPerson = null;
+
+    let btnClass = '';
     
     if (this.state.showPersons) {
       displayPerson = (
@@ -117,6 +119,8 @@ class App extends Component {
             })}
         </div> 
       );
+      
+      btnClass = classes.Red;
       
       // style.backgroundColor = 'Red';
       
@@ -149,21 +153,21 @@ class App extends Component {
       
     //}
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     };
     if (this.state.persons.length <=1 ){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     };
 
     return(
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className= {classes.join(' ')}>This is really working</p>
+          <p className= {assignedClasses.join(' ')}>This is really working</p>
           {/* <StyledButton alt = {this.state.showPersons}  */}
-          <button className = "button"
-            onClick={this.togglePersonDivHandler}>Ganon Wins </button>
+          <button className = {btnClass}
+            onClick={this.togglePersonDivHandler}>Ganon Wins</button>
           {/* </StyledButton>  */}
           {displayPerson}
         </div>
