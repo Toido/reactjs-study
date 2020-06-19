5561c31c8b0bc46d721de8ed43b1a31a0e37db3a
darylcaguia <caguiadaryl@gmail.com>
2020-06-19 17:46:04 +0800

    Added Reference for Element/s

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -19,14 +19,31 @@ import withClass from '../../../hoc/withClass';
 
 
 class Person extends Component { 
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
             //<div className = {classes.Person}>
             <Aux>
-                <p onClick = {this.props.click}>Hi, I'm {this.props.name} and I'm {this.props.age} years old!</p>
+                <p onClick = {this.props.click}>
+                    Hi, I'm {this.props.name} and I'm {this.props.age} years old!
+                </p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange ={this.props.change} value = {this.props.name}></input>
+                <input type="text"
+                // ref = {(inputEl) => {this.inputElement = inputEl}} //Old approach of creating reference for elements
+                ref = {this.inputElementRef} //Modern approach of creating reference for elements
+                onChange ={this.props.change} 
+                value = {this.props.name}
+                />
             </Aux>
             //</div>
         );
